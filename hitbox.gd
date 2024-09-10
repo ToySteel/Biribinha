8b21cc968e05b5fa460de4d9b9cedb56b65a787d
ToySteel <equipesesi.toyssteel@gmail.com>
2024-09-10 12:17:24 -0300

    2

--- hitbox.gd
+++ hitbox.gd
@@ -1,11 +1,13 @@
 extends Area2D
 signal hurt
-var teste
+var teste = true
 @onready var timer = $Timer
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		body.velocity.y = body.JUMP_FORCE
-		owner.anim.play("hurt")
-		$Timer.start()
+		if teste:
+			body.velocity.y = body.JUMP_FORCE
+			owner.anim.play("hurt")
+			$Timer.start()
+			teste = false
 func _on_timer_timeout() -> void:
 	owner.queue_free()
