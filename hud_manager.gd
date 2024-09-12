7e371173f8b3b82cbae3b57ad56a43fe21d97864
ToySteel <equipesesi.toyssteel@gmail.com>
2024-09-12 10:22:05 -0300

    12/09-02

--- hud_manager.gd
+++ hud_manager.gd
@@ -1,12 +1,16 @@
 extends Control
 @onready var timer_counter: Label = $container/timer_container/timer_counter as Label
 @onready var life_counter: Label = $"container/life_container/life counter" as Label
-
+@onready var clock_timer: Timer = $clock_timer as Timer
+var minutes = 0
+var seconds = 0 
+@export_range(0,5) var default_minutes := 1
+@export_range(0,59) var default_seconds := 0
 
 
 func _ready() -> void:
 	life_counter.text = str("%02d" % Globals.player_life)
-	
+	timer_counter.text = str("%02d" % default_minutes) + ":" + str("%02d" % default_seconds)
 	
 	
 
@@ -16,3 +20,11 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	life_counter.text = str("%02d" % Globals.player_life)
+
+
+func _on_clock_timer_timeout() -> void:
+	if seconds > 59:
+		minutes +=1
+		seconds = 0
+	seconds += 1
+	timer_counter.text = str("%02d" % minutes) + ":" + str("%02d" % seconds)
--- word_00.gd
+++ word_00.gd
@@ -1,11 +1,12 @@
 extends Node2D
-
+@onready var clock_timer: Timer = $HUD/control/clock_timer
 @onready var player := $player
 @onready var camera := $Camera
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.follow_camera(camera)
 	player.player_has_died.connect(reload_game)
+	Globals.player_life = 3
 
 
 
